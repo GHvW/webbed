44923fc0b8b95e21f238606fc3cb2102499d752c
Garrett van Wageningen <ghvanw283@gmail.com>
2020-02-26 19:53:38 -0600

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to webbed
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/webbed/core_test.clj
@@ -0,0 +1,7 @@
+(ns webbed.core-test
+  (:require [clojure.test :refer :all]
+            [webbed.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
